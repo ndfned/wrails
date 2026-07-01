342701b5af7efa1bdcfeeae857d95bd497fe3d27
ndfnd <hellpl4y@gmail.com>
2026-07-01 16:27:05 +0500

    add basic test

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 test:
-	ruby test/wrails_test.rb
+	ruby test/wrails_test2.rb
 
 # make testx TEST=test_post_request
 testx:
 	ruby test/wrails_test.rb -n $(TEST)
 
 example:
-	ruby examples/basic.rb
+	ruby examples/full_framework/app.rb
 
 .PHONY: test
---
+++ test/wrails_test2.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper'
+
+class TestController < Wrails::Controller
+  def index
+    'Home Page'
+  end
+end
+
+class WrailsTest < Minitest::Test
+  def setup
+    # Wrails::Routes.clear_routes!
+    # Wrails::Config.views_path = 'test/files/views'
+  end
+
+  def assert_body(expected, response)
+    assert_equal expected, response.body
+  end
+
+  def get(path)
+    Wrails.app.call(Rack::MockRequest.env_for(path, method: 'GET'))
+  end
+
+  def test_get_request_to_existing_route
+    Wrails.app.router.add_route(:get, '/test', 'test#index')
+    assert_body 'Home Page', get('/test')
+  end
+end
